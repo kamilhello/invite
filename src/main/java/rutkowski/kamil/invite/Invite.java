bd655c26a70a86ad75e29bfb9a99f0275d3e85a1
kamil.hello <kamil.mymail@protonmail.com>
2022-09-26 21:48:05 +0200

    Add IllegalArgumentException and tests

--- src/main/java/rutkowski/kamil/invite/Invite.java
+++ src/main/java/rutkowski/kamil/invite/Invite.java
@@ -2,10 +2,24 @@ package rutkowski.kamil.invite;
 
 public class Invite {
 
-    public static boolean inviteMoreWomen(int[] listOfGoers) {
+    public static boolean inviteMoreWomen(int[] listOfGoers) throws IllegalArgumentException{
+        
+        if(listOfGoers == null){
+            String excMsg = "listOfGoers can't be null";
+            throw new IllegalArgumentException(excMsg);
+        }
+        else if(listOfGoers.length < 2 || listOfGoers.length > 50){
+            String excMsg = "Length of listOfGoers must be from 2 to 50!";
+            throw new IllegalArgumentException(excMsg);
+        }
+
         int balance = 0;
 
         for(int goerGender: listOfGoers){
+            if(goerGender != 1 && goerGender != -1){
+                String excMsg = "Values of listOfGoers should be 1 or -1!";
+                throw new IllegalArgumentException(excMsg);
+            }
             balance += goerGender;
         }
 
--- src/test/java/rutkowski/kamil/InviteTest.java
+++ src/test/java/rutkowski/kamil/InviteTest.java
@@ -4,23 +4,81 @@ import rutkowski.kamil.invite.Invite;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+
+import java.util.List;
+import java.util.Arrays;
 
 public class InviteTest {
+    private static List<int[]> goersLists() {
+
+        int[] goersList1 = {};
+        int[] goersList2 = {-1};
+        int[] goersList3 = new int[51];
+
+        for(int i=0;i<goersList3.length;i++){
+            goersList3[i] = 1;
+        }
+
+        return Arrays.asList(goersList1,goersList2,goersList3);
+    }
+
+    @ParameterizedTest
+    @MethodSource("goersLists")
+    public void testInputArraySize(int[] listOfGoers){
+        
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Invite.inviteMoreWomen(listOfGoers));
+
+        String excMsg = exception.getMessage();
+        assert excMsg != null : "Exception message should not be null"; 
+
+        String msg = "Exception message should contain the word \"length\"";
+        assert excMsg.toLowerCase().contains("length") : msg;
+    }
+
+    @ParameterizedTest
+    @NullSource
+    public void testInputForNull(int[] listOfGoers){
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Invite.inviteMoreWomen(listOfGoers));
+
+        String excMsg = exception.getMessage();
+        assert excMsg != null : "Exception message should not be null"; 
+
+        String msg = "Exception message should contain the word \"null\"";
+        assert excMsg.toLowerCase().contains("null") : msg;
+    }
+
+    @Test
+    public void testInputArrayValues(){
+
+        int[] listOfGoers = {1,-1,4,1,-5,3,-1};
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> Invite.inviteMoreWomen(listOfGoers));
+
+        String excMsg = exception.getMessage();
+        assert excMsg != null : "Exception message should not be null"; 
+
+        String msg = "Exception message should contain the word \"values\"";
+        assert excMsg.toLowerCase().contains("values") : msg;
+    }
 
     @Test
     public void shouldAnswerWithTrue(){
-        int[] goers = {1,-1,1,1,1,-1};
+        int[] listOfGoers = {1,-1,1,1,1,-1};
 
-        assertTrue(Invite.inviteMoreWomen(goers));
+        assertTrue(Invite.inviteMoreWomen(listOfGoers));
     }
 
     @Test
     public void shouldAnswerWithFalse(){
-        int[] goers = {-1,-1,-1,-1,-1,-1};
+        int[] listOfGoers = {-1,-1,-1,-1,-1,-1};
 
-        assertFalse(Invite.inviteMoreWomen(goers));
+        assertFalse(Invite.inviteMoreWomen(listOfGoers));
     }
     
 }
